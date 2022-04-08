83da58bda0c193e44d4b92c33467996a7c1ebc97
Misha En <mishaenn@gmail.com>
2022-04-08 15:53:42 +0300

    Добавил в конфиг описание подключения к бд

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,13 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=pgsql;dbname=yii2advanced',
+            'username' => 'yii2advanced',
+            'password' => 'secret',
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
